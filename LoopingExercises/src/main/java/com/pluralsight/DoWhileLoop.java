f87f75eaa5aa38b211bff376bdeddb6cd90986b7
James Bosley <jamesbosley123@gmail.com>
2025-04-23 09:36:14 -0500

    Adding looping exercises

---
+++ LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int j = 0;
+        do {
+            System.out.println("I love Java");
+            j++;
+        } while(j < 5);
+    }
+}
---
+++ LoopingExercises/src/main/java/com/pluralsight/LoopExercisesApp.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class LoopExercisesApp {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+        while(i < 5) {
+            i++;
+            System.out.println("I love Java");
+        }
+    }
+}
